141efab26ea78a1b482f499ea3dd6740c1a7e8aa
ashrafa <ashraf.a@spartadigital.co.uk>
2018-01-24 18:02:25 +0000

    Added screenshot working

--- app/item/items.component.ts
+++ app/item/items.component.ts
@@ -2,11 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import firebase = require("nativescript-plugin-firebase");
 import { Item } from "./item";
 import { ItemService } from "./item.service";
-import { ImageAsset } from 'tns-core-modules/image-asset';
-import { ImageSource } from 'tns-core-modules/image-source';
-import * as Camera from 'nativescript-camera';
-import * as fs from 'tns-core-modules/file-system';
-import * as SocialShare from 'nativescript-social-share';
 
 @Component({
     selector: "ns-items",
@@ -16,18 +11,10 @@ import * as SocialShare from 'nativescript-social-share';
 export class ItemsComponent implements OnInit {
     items: Item[];
     public user: any;
-    public picture: any;
-    public imageTaken: ImageAsset;
-    public imageFile: ImageSource;
-    public saveToGallery: boolean = true;
-    public keepAspectRatio: boolean = true;
-    public width: number = 300;
-    public height: number = 300;
 
     // This pattern makes use of Angular’s dependency injection implementation to inject an instance of the ItemService service into this class. 
     // Angular knows about this service because it is included in your app’s main NgModule, defined in app.module.ts.
     constructor(private itemService: ItemService) {
-        this.picture = "https://placehold.it/200x200";
      }
 
     ngOnInit(): void {
@@ -85,40 +72,4 @@ export class ItemsComponent implements OnInit {
         );
     }
 
-    public takePicture() {
-        let options = {
-            width: this.width,
-            height: this.height,
-            keepAspectRatio: this.keepAspectRatio
-//            saveToGallery: this.saveToGallery
-        };
-        Camera.takePicture(options).then(pic => {
-            console.dir(pic);
-            this.imageTaken = pic;
-            console.log(pic.android);
-            let source = new ImageSource();
-            source.fromAsset(pic).then(source => {
-                console.log(`source`);
-                console.dir(source);
-                this.imageFile = source;
-                let folder = fs.knownFolders.documents().path;
-                console.log(folder);
-                let date = new Date();
-                let filename = "ss-" + date.getTime().toString() + ".png";
-                console.log(filename);
-                let file = fs.path.join(folder, filename);
-                let saved = source.saveToFile(file, "png");
-                if (saved) {
-                    console.log(`saved img : ${file}`);
-                    this.picture = file;
-                }
-            });
-        }).then(() => {
-            SocialShare.shareImage(this.imageFile);
-        });
-    }
-
-    public shareThis() {
-        //SocialShare.shareImage(this.imageFile);
-    }
 }
--- app/item/sharepic.component.ts
+++ app/item/sharepic.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
 import { ImageAsset } from 'tns-core-modules/image-asset';
 import { ImageSource } from 'tns-core-modules/image-source';
-import * as Camera from 'nativescript-camera';
+import { Image } from 'tns-core-modules/ui/image';
 import * as fs from 'tns-core-modules/file-system';
+import * as Camera from 'nativescript-camera';
 import * as SocialShare from 'nativescript-social-share';
+import * as Screenshot from 'nativescript-screenshot';
+import { Page } from 'tns-core-modules/ui/page';
 
 @Component({
     selector: "sharepic",
@@ -19,7 +22,8 @@ export class SharepicComponent {
     public width: number = 300;
     public height: number = 300;
 
-    constructor() {
+    constructor(private page: Page) {
+        
         this.picture = "https://placehold.it/200x200";
      }
 
@@ -31,13 +35,13 @@ export class SharepicComponent {
 //            saveToGallery: this.saveToGallery
         };
         Camera.takePicture(options).then(pic => {
-            console.dir(pic);
+            // console.dir(pic);
             this.imageTaken = pic;
             console.log(pic.android);
             let source = new ImageSource();
             source.fromAsset(pic).then(source => {
                 console.log(`source`);
-                console.dir(source);
+                // console.dir(source);
                 this.imageFile = source;
                 let folder = fs.knownFolders.documents().path;
                 console.log(folder);
@@ -57,6 +61,23 @@ export class SharepicComponent {
     }
 
     public shareThis() {
-        //SocialShare.shareImage(this.imageFile);
-    }     
+        if (this.imageFile) SocialShare.shareImage(this.imageFile);
+    }
+    
+    public screenshotThis() {
+        let shotImg = new Image();
+        shotImg.imageSource = Screenshot.getImage(this.page);
+        this.imageFile = shotImg.imageSource;
+
+        let folder = fs.knownFolders.documents().path;
+        let date = new Date();
+        let filename = "ss-" + date.getTime().toString() + ".png";
+        console.log(filename);
+        let file = fs.path.join(folder, filename);
+        let saved = this.imageFile.saveToFile(file, "png");
+        if (saved) {
+            console.log(`saved img : ${file}`);  
+            this.picture = file;          
+        }       
+    }
 }
